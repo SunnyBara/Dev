794f58f8fd30e1f64e39f9737b8fc936ee447abd
SunnyBara <115371756+SunnyBara@users.noreply.github.com>
2023-10-08 19:18:05 +0200

    Delete jc05 directory

--- jc05/tp/mystrlen.c
+++
@@ -1,9 +0,0 @@
-int mystrlen(const char *s)
-{
-    char i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return(i);
-}
--- jc05/tp/swap.c
+++
@@ -1,7 +0,0 @@
-void swap(int  *a, int *b)
-{
-    int c = a[0];
-    a[0] = b[0];
-    b[0] = c;
-    return;
-}
