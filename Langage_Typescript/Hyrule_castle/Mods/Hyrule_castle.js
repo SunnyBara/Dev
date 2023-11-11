5bc381b79ce782fd999b4f4dbd778fac761143ec
SunnyBara <115371756+SunnyBara@users.noreply.github.com>
2023-11-11 22:56:32 +0100

    [remove] tous les js et nodes modules

--- Langage_Typescript/Hyrule_castle/Mods/Hyrule_castle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var start_1 = require("./Start/start");
-function Hyrule_castle() {
-    (0, start_1.Start)();
-}
-exports.default = Hyrule_castle;
--- Langage_Typescript/Hyrule_castle/Mods/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var start_1 = require("./Start/start");
-(0, start_1.Start)();
