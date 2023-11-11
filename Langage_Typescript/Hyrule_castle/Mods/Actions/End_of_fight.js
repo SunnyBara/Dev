d3bcbdfb8d47a01c87a87d29399c77e0bf517ad7
SunnyBara <115371756+SunnyBara@users.noreply.github.com>
2023-11-11 22:52:48 +0100

    [remove] tous les js et nodes modules

--- Langage_Typescript/Hyrule_castle/Mods/Actions/End_of_fight.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.End_of_fight = void 0;
-function End_of_fight() {
-}
-exports.End_of_fight = End_of_fight;
--- Langage_Typescript/Hyrule_castle/Mods/data/Tower.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tower = void 0;
-var Tower = /** @class */ (function () {
-    function Tower() {
-    }
-    return Tower;
-}());
-exports.Tower = Tower;
